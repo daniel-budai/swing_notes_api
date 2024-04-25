dfcdc2f267c47bf0a359813952670adf8e7f0c66
daniel-budai <danielbudai@tutanota.com>
2024-04-25 11:24:07 +0200

    continue routes

--- Routes/noteRoutes.js
+++ Routes/noteRoutes.js
@@ -1,4 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  //Hämta anteckningar
+});
+
+router.post("/", (req, res) => {
+  //Spara en anteckning
+});
+
+router.put("/", (req, res) => {
+  //Ändra en anteckning
+});
+
+router.delete("/", (req, res) => {
+  //Ta bort en anteckning
+});
+
+router.get("/search", (req, res) => {
+  //Söka bland anteckningar (VG-krav). Sökning sker på titel.
+});
+
 module.exports = router;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/signup", (req, res) => {
+  //registrera en användare
+  // /api/user/signup
+});
+
+router.post("/login", (req, res) => {
+  //logga in en användare
+  // /api/user/login
+});
+
 module.exports = router;
