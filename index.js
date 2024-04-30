a2dac3bfc8ebf4f2c9f4280cab025d4c33313135
daniel-budai <danielbudai@tutanota.com>
2024-04-30 11:24:31 +0200

    cors, and swagger

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 const userRouter = require("./Routes/userRoutes");
 const notesRouter = require("./Routes/noteRoutes");
 
+const setupSwagger = require("./Swagger/swagger.js");
+setupSwagger(app);
+
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/notes", notesRouter);
-
-/*app.get("/", (req, res) => {
-  res.send("Hello, world!");
-}); */
-/*const port = process.env.PORT || 3000;
-const baseUrl = process.env.BASE_URL || "localhost"; */
 
 app.listen(process.env.PORT, process.env.BASE_URL, () => {
   console.log(
